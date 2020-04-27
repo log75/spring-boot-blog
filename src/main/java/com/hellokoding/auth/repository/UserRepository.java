5d79e6374551543b71bcc1c3d21625f3bc63ca93
alirexa <aliyyyyreza@gmail.com>
2020-04-27 19:39:06 +0430

    first

---
+++ src/main/java/com/hellokoding/auth/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.hellokoding.auth.repository;
+
+import com.hellokoding.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    @Modifying
+    @Query(value = "insert into user_roles (users_id, roles_id) VALUES (:id , 1)", nativeQuery = true)
+    void insertIntoUserRole(@Param("id") Long id);
+
+}
